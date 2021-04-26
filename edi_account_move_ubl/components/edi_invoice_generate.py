0534a2a8489895ab95507b9f5073e1a431ef66dc
RaulOForgeFlow <raul.ortiz@forgeflow.com>
2021-04-26 13:53:40 +0200

    edi generate add description

--- edi_account_move_ubl/components/edi_invoice_generate.py
+++ edi_account_move_ubl/components/edi_invoice_generate.py
@@ -3,6 +3,7 @@ from odoo.addons.component.core import Component
 class EdiBookGenerate(Component):
     _name = "edi.output.generate.invoice"
     _inherit = "edi.component.output.mixin"
+    _description = "Generates the book in edi format"
     _usage = "output.generate"
     _backend_type = "invoice"
 
