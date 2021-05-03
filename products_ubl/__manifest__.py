3538f6d675534a1952e0e932a1404e7190e772d6
FerranCforgeFlow <ferran.coll@forgeflow.com>
2021-05-03 12:27:33 +0200

    Add product_ubl module

---
+++ products_ubl/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2016-2017 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Catalog UBL",
+    "version": "13.0.1.0.0",
+    "category": "Product",
+    "license": "AGPL-3",
+    "summary": "Generate UBL XML file of a product catalog",
+    "author": "Ferran Coll,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi/",
+    "depends": ["product", "base_ubl"],
+    "data": [],
+    "installable": True,
+}
